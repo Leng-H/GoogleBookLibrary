9bba4bb283b02d1bad4dceea077e97a6554f0535
Sivleng H <sivlenghul@sivlengs-mini.lan>
2024-08-17 15:59:58 -0400

    Temporarily adding a valid href

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="https://www.google.com">
           Navbar
         </a>
         <button
@@ -32,22 +32,26 @@ function Navbar() {
           </form>
           <ul class="navbar-nav mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="https://www.google.com"
+              >
                 Home
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="https://www.google.com">
                 Sign In
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="https://www.google.com">
                 Blog
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="https://www.google.com">
                 Donate
               </a>
             </li>
