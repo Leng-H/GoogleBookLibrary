b1b1a75ceb6f2406fa9bdcbeb7bf30d19cb03a08
Sivleng H <sivlenghul@sivlengs-mini.lan>
2024-08-17 15:36:45 -0400

    Adding a NavBar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,12 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="Home">
+    <div>
       <h1>Welcome to library!</h1>
+      {/* TODO: Nav bar */}
+      {/* TODO: Move SearchBar into NavBar, and only expands on click */}
       <SearchBar />
+      {/* TODO: Displaying the book */}
     </div>
   );
 }
